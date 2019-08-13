740206d552b21ab4ad0c88b52049027ab8574603
Ionut Ivan <ionutzi@gmail.com>
2019-08-13 21:42:10 +0200

    add react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>, document.getElementById('app'))
